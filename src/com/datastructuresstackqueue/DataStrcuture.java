61404c0242ff8166d1c9113cbc42f0ac59f7bd01
AdityaS98 <adityasharmalko1998@gmail.com>
2023-05-01 12:56:15 +0530

    Added : A a program for deque a stack for the given set of values enterd by the user

--- src/com/datastructuresstackqueue/DataStrcuture.java
+++ src/com/datastructuresstackqueue/DataStrcuture.java
@@ -1,8 +1,12 @@
 package com.datastructuresstackqueue;
+/*
+ * This is a main class of program
+ */
 
 /*
  * Node class for node and data
  */
+
 class Node {
 
     int data;
@@ -12,15 +16,14 @@ class Node {
 /*
  * Class to define stack operations method in it
  */
-class StackNew {
+class StackNew2 {
 
     Node top;
 
     /*
      * To define constructor which save value of top of stack
      */
-
-    public StackNew() {
+    public StackNew2() {
         super();
         this.top = top;
     }
@@ -42,18 +45,51 @@ class StackNew {
             System.out.println("Stack Overflow");
             return;
         }
-
         temp.data = x;
         temp.link = top;
         top = temp;
     }
 
     /*
-     * To display stack
+     * To check stack is empty or not
      */
-    public void display() throws Throwable {
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public int peek() {
+        /*
+         * check for empty stack
+         */
+        if (!isEmpty()) {
+            return top.data;
+        } else {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+    }
+
+    /*
+     * Utility function to pop top element from the stack
+     */
+    public void pop() {
         /*
          * To check for stack underflow
+         */
+        if (top == null) {
+            System.out.println("Stack Underflow");
+            return;
+        }
+
+        /*
+         * To update the top pointer to point to the next node
+         */
+        top = (top).link;
+    }
+
+    public void display() throws Throwable {
+        /*
+         * check for stack underflow
          */
         if (top == null) {
             System.out.println("Stack Underflow");
@@ -71,32 +107,38 @@ class StackNew {
                  * assign temp link to temp
                  */
                 temp = temp.link;
-                if (temp != null) System.out.print(" -> ");
+                if (temp != null)
+                    System.out.print(" -> ");
             }
         }
     }
 }
-
-/*
- * This is a main class of program
- */
 public class DataStrcuture {
+
     public static void main(String[] args) throws Throwable {
 
         /*
          * To create stack class object
          */
-        StackNew stack1 = new StackNew();
 
-        stack1.push(70);
-        stack1.push(30);
-        stack1.push(56);
+        StackNew2 stack2 = new StackNew2();
 
-        stack1.display();
+        stack2.push(70);
+        stack2.push(30);
+        stack2.push(56);
+
+        System.out.println(stack2.peek());
+        stack2.pop();
+        System.out.println(stack2.peek());
+        stack2.pop();
+        System.out.println(stack2.peek());
+        stack2.pop();
+        System.out.println(stack2.peek());
 
     }
-
 }
+
+
 
 
 
